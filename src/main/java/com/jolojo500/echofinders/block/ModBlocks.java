c1518235817c5e2f22c3dcb0b32062dfa0b2e61a
jolojo500 <djenadianis05@gmail.com>
2025-01-02 22:59:35 -0500

    basic initial setup for ModBlocks

---
+++ src/main/java/com/jolojo500/echofinders/block/ModBlocks.java
@@ -0,0 +1,10 @@
+package com.jolojo500.echofinders.block;
+
+import com.jolojo500.echofinders.EchoFindersMod;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModBlocks {
+    public static DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(EchoFindersMod.MODID);
+
+
+}
